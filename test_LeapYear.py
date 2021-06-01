a0ccb8d384edd3e1bdfde2110bc0361d5437c1d8
Forrest <forrest.90h@gmail.com>
2021-05-31 22:20:46 -0700

    added test for mod 100

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -1,9 +1,13 @@
 def leapYear(year):
-    if year%4==0:
+    if year % 4 == 0:
         return True
     else:
         return False
 
+
 class TestClass:
     def test_one(self):
         assert leapYear(2021) == False
+
+    def test_two(self):
+        assert leapYear(2100) == True
