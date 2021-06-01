d0169bcc856e478a3850fd4f5f5c06de61c879cd
Forrest <forrest.90h@gmail.com>
2021-05-31 21:46:02 -0700

    wrote code for fizz

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -1,14 +1,7 @@
 def FizzBuzz():
     for i in range(0, 100):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
+        if i % 3 == 0:
             print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
-
 
 class TestClass:
     def test_one(self, capsys):
