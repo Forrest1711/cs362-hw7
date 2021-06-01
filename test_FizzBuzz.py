0c09b01566e55345ec7117342424ce331fbf8779
Forrest <forrest.90h@gmail.com>
2021-05-31 20:44:29 -0700

    added test for occurance of fizz output

---
+++ test_FizzBuzz.py
@@ -0,0 +1,18 @@
+class TestClass:
+    def test_one(self, capsys):
+        FizzBuzz()
+        out, err = capsys.readouterr()
+
+        fizzCount = 0
+        buzzCount = 0
+        fizzBuzzCount = 0
+
+        for i in range(0, len(out)):
+            if(out[i] == 'F'):
+                if(out[i+4] == 'B'):
+                    fizzBuzzCount += 1
+                else:
+                    fizzCount += 1
+            elif(out[i] == 'B' and out[i-1] != 'z'):
+                buzzCount += 1
+        assert fizzCount == 33
