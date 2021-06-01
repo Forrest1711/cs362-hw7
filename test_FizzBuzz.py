0e4435fc69fd2764002598da9be5399fa910b59e
Forrest <forrest.90h@gmail.com>
2021-05-31 21:53:01 -0700

    added test for buzz

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -1,7 +1,8 @@
 def FizzBuzz():
     for i in range(0, 100):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 != 0:
             print("Fizz")
+
 
 class TestClass:
     def test_one(self, capsys):
@@ -9,15 +10,20 @@ class TestClass:
         out, err = capsys.readouterr()
 
         fizzCount = 0
+
+        for i in range(0, len(out)):
+            if(out[i] == 'F' and out[i+4] != 'B'):
+                fizzCount += 1
+        assert fizzCount == 27
+
+    def test_two(self, capsys):
+        FizzBuzz()
+        out, err = capsys.readouterr()
+
         buzzCount = 0
         fizzBuzzCount = 0
 
         for i in range(0, len(out)):
-            if(out[i] == 'F'):
-                if(out[i+4] == 'B'):
-                    fizzBuzzCount += 1
-                else:
-                    fizzCount += 1
-            elif(out[i] == 'B' and out[i-1] != 'z'):
+            if(out[i] == 'B' and out[i-1] != 'z'):
                 buzzCount += 1
-        assert fizzCount == 27
+        assert buzzCount == 14
